5feba0d07018a9e217ae0685a191576565ff1e3e
zsj <zousjun@126.com>
2023-01-18 16:12:11 +0800

    开发完成

--- gateway/main.go
+++ gateway/main.go
@@ -5,6 +5,7 @@ import (
 	authpb "coolcar/auth/api/gen/v1"
 	rentalpb "coolcar/rental/api/gen/v1"
 	"coolcar/shared/server"
+	"flag"
 	"log"
 	"net/http"
 
@@ -12,30 +13,15 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/encoding/protojson"
 )
+var addr = flag.String("addr", ":8080", "address listen")
+var authAddr = flag.String("auth_addr", "localhost:8081", "address for auth service")
+var tripAddr = flag.String("trip_addr", "localhost:8082", "address for trip service")
+var profileAddr = flag.String("profile_addr", "localhost:8082", "address for profile service")
+var carAddr = flag.String("car_addr", "localhost:8084", "address for car service")
 
 func main() {
+	flag.Parse()
 	log.SetFlags(log.Lshortfile)
-	startGRPCGateway()
-	// // 监听端口
-	// lis, err := net.Listen("tcp", ":8081")
-	// if(err != nil) {
-	// 	log.Fatalf("failed to listen: %v", err)
-	// }
-	// // 创建grpc服务
-	// grpcServer := grpc.NewServer()
-	
-	// authpb.RegisterAuthServiceServer(grpcServer, &auth.Service{})
-	
-	// // if err != nil {
-	// // 	fmt.Println("服务启动失败", err)
-	// // 	return
-	// // }
-	// log.Fatal(grpcServer.Serve(lis))
-	// fmt.Println("连接成功")
-
-}
-
-func startGRPCGateway() {
 	lg, err := server.NewZapLogger();
 	if err != nil {
 		log.Fatalf("cannot create zap:logger: %v", err)
@@ -86,19 +72,107 @@ func startGRPCGateway() {
 			log.Fatalf("cannot register service %s: %v",s.name,err)
 		}
 	}
-	// err := authpb.RegisterAuthServiceHandlerFromEndpoint(c, mux, ":8081", []grpc.DialOption{grpc.WithInsecure()})
-	// if(err != nil) {
-	// 	log.Fatalf("cannot start grpc gateway %v", err)
-	// }
-	// err = rentalpb.RegisterTripServiceHandlerFromEndpoint(c, mux, ":8082", []grpc.DialOption{grpc.WithInsecure()})
-	// if(err != nil) {
-	// 	log.Fatalf("cannot start grpc gateway 8082 %v", err)
-	// }
-	// 8080的请求分发到8081的服务器上面
-	addr := ":8080";
 	lg.Sugar().Infof("grpc server started ad: %s", addr);
-	err = http.ListenAndServe(addr, mux);
+	err = http.ListenAndServe(*addr, mux);
 	if err != nil {
 		log.Fatalf("cannot listen to 8080 %v", err)
 	}
+	
+
 }
+
+package main
+
+import (
+	"context"
+	authpb "coolcar/auth/api/gen/v1"
+	carpb "coolcar/car/api/gen/v1"
+	rentalpb "coolcar/rental/api/gen/v1"
+	"coolcar/shared/auth"
+	"coolcar/shared/server"
+	"log"
+	"net/http"
+	"net/textproto"
+
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/namsral/flag"
+	"google.golang.org/grpc"
+)
+
+var addr = flag.String("addr", ":8080", "address to listen")
+var authAddr = flag.String("auth_addr", "localhost:8081", "address for auth service")
+var tripAddr = flag.String("trip_addr", "localhost:8082", "address for trip service")
+var profileAddr = flag.String("profile_addr", "localhost:8082", "address for profile service")
+var carAddr = flag.String("car_addr", "localhost:8084", "address for car service")
+
+func main() {
+	flag.Parse()
+
+	lg, err := server.NewZapLogger()
+	if err != nil {
+		log.Fatalf("cannot create zap logger: %v", err)
+	}
+	c := context.Background()
+	c, cancel := context.WithCancel(c)
+	defer cancel()
+
+	mux := runtime.NewServeMux(runtime.WithMarshalerOption(
+		runtime.MIMEWildcard, &runtime.JSONPb{
+			EnumsAsInts: true,
+			OrigName:    true,
+		},
+	), runtime.WithIncomingHeaderMatcher(func(key string) (string, bool) {
+		if key == textproto.CanonicalMIMEHeaderKey(runtime.MetadataHeaderPrefix+auth.ImpersonateAccountHeader) {
+			return "", false
+		}
+		return runtime.DefaultHeaderMatcher(key)
+	}))
+
+	serverConfig := []struct {
+		name         string
+		addr         string
+		registerFunc func(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error)
+	}{
+		{
+			name:         "auth",
+			addr:         *authAddr,
+			registerFunc: authpb.RegisterAuthServiceHandlerFromEndpoint,
+		},
+		{
+			name:         "trip",
+			addr:         *tripAddr,
+			registerFunc: rentalpb.RegisterTripServiceHandlerFromEndpoint,
+		},
+		{
+			name:         "profile",
+			addr:         *profileAddr,
+			registerFunc: rentalpb.RegisterProfileServiceHandlerFromEndpoint,
+		},
+		{
+			name:         "car",
+			addr:         *carAddr,
+			registerFunc: carpb.RegisterCarServiceHandlerFromEndpoint,
+		},
+	}
+
+	for _, s := range serverConfig {
+		err := s.registerFunc(
+			c, mux, s.addr,
+			[]grpc.DialOption{grpc.WithInsecure()},
+		)
+		if err != nil {
+			lg.Sugar().Fatalf("cannot register service %s: %v", s.name, err)
+		}
+	}
+	http.HandleFunc("/healthz", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write([]byte("ok"))
+	})
+	http.Handle("/", mux)
+	lg.Sugar().Infof("grpc gateway started at %s", *addr)
+	lg.Sugar().Fatal(http.ListenAndServe(*addr, nil))
+}
+
+
+// func startGRPCGateway() {
+	
+// }
