3ce811c564b1bf26d9d7d70af7f54c38130f0486
zsj <zousjun@126.com>
2023-01-19 17:50:52 +0800

    add

---
+++ OldPackageTest/go.mod
@@ -0,0 +1,8 @@
+module OldPackageTest
+
+go 1.19
+
+require (
+	google.golang.org/grpc v1.52.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ back.sh
@@ -0,0 +1,1 @@
+ protoc -I=. --go_out=paths=source_relative:.  --go-grpc_out=require_unimplemented_servers=false,paths=source_relative:. ./OldPackageTest/grpc_token_auth_test/proto/helloworld.proto 
